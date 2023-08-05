9a13332bd898fd143da3b50cd735f68475c38e34
JEXB <jippee.1211@gmail.com>
2023-08-05 18:08:00 +0700

    Lab12

--- src/app/page.js
+++ src/app/page.js
@@ -78,7 +78,11 @@ export default function HomePage() {
           <Controller />
         </LangContext.Provider>
       </ThemeContext.Provider>
-      <Footer year="2023" fullName="Chayanin Suatap" studentId="650610560" />
+      <Footer
+        year="2023"
+        fullName="suwichada pongkanmoon"
+        studentId="630610657"
+      />
     </div>
   );
 }
--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,13 +1,15 @@
+import { LangContext } from "@/contexts/LangContext";
 import { ThemeContext } from "@/contexts/ThemeContext";
 import { useContext } from "react";
 
 export const Detail = () => {
   const { theme } = useContext(ThemeContext);
+  const { lang } = useContext(LangContext);
   return (
     <p className="text-center" style={{ color: theme.fgColor }}>
-      Display activated theme message here (EN or TH)
-      {/* tip: Get information from "theme" variable to determine 
-      which theme is currently selected */}
+      {theme.name === "light"
+        ? lang.detail.lightActivated
+        : lang.detail.darkActivated}
     </p>
   );
 };
